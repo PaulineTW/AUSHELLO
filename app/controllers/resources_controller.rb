f39e20b46222e042b7984557b1fd2fbcc40d1ee7
pauline rouger <paulinerouge@hotmail.com>
2021-12-16 17:28:01 +0800

    generate resources controller + user seed

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,7 @@
+class ResourcesController < ApplicationController
+
+  def index
+    @resources = Resource.all
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning users from the database...'
+
+User.destroy_all
+
+puts 'Creating 3 users ...'
+user1 = User.create!(email: 'admin@admin.com', password: 'password')
+user2 = User.create!(email: 'julianne@user1.com', password: 'password')
+user3 = User.create!(email: 'pauline@user2.com', password: 'password')
+
+puts "Created #{User.count} users ..."
