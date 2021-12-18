f28c1d9719046f0f273a2068699d5159c8def1f1
pauline rouger <paulinerouge@hotmail.com>
2021-12-18 13:35:44 +0800

    added Mapbox to Index view + markers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +33,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
