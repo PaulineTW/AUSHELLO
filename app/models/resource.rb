1371363218ac9da4ac19b014f7be587a9dbe4052
pauline rouger <paulinerouge@hotmail.com>
2021-12-18 13:15:24 +0800

    run test for Resource.create OK

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,4 +4,6 @@ class Resource < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, :category, :address, :website, :phone, :state, :email, :description, presence: true
   validates :category, inclusion: { in: ["Legal", "Health", "Housing", "Education", "Visa", "Employment", "Every day" ]}
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
