a74b1452655bb7662339c88294b34503f36a3a7b
Julianne Tran <julianne.tran03@gmail.com>
2021-12-18 14:37:14 +1100

    changed the gem 'dotenv-rails', groups: [:development, :test]

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,15 +2,15 @@ class FavouritesController < ApplicationController
 
   def create
     @resource = Resource.find(params[:resource_id])
-    # if @resource.user_id != current_user.id   #You can register favorites other than your own resources
-    @favorite = Favourite.create(user_id: current_user.id, resource_id: @resource.id)
-    # end
+    # if @resource.user_id != current_user.id   #You can register favourites other than your own resources
+    # @favourite = Favourite.create(user_id: current_user.id, resource_id: @resource.id)
+    # # end
   end
 
   def destroy
     @resource = resource.find(params[:resource_id])
-    @favorite = Favourite.find_by(user_id: current_user.id, resource_id: @resource.id)
-    @favorite.destroy
+    # @favourite = Favourite.find_by(user_id: current_user.id, resource_id: @resource.id)
+    # @favourite.destroy
   end
 
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,6 @@
 class Resource < ApplicationRecord
   belongs_to :user
-  has_many :favourites, dependent: :destroy
+  # has_many :favourites, dependent: :destroy
   validates :name, uniqueness: true
   validates :name, :category, :address, :website, :phone, :state, :email, :description, presence: true
   validates :category, inclusion: { in: ["Legal", "Health", "Housing", "Education", "Visa", "Employment", "Every day" ]}
