9e8415fa1149daecfc760340c06b7c454319bf9c
Julianne Tran <julianne.tran03@gmail.com>
2021-12-22 22:56:30 +1100

    Add column category3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_114912) do
+ActiveRecord::Schema.define(version: 2021_12_22_115314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_114912) do
     t.float "latitude"
     t.float "longitude"
     t.string "category2"
+    t.string "category3"
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
