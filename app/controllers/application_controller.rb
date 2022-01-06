9292ab04452828390bc864d0f0b6f25414a62678
elliott fuller <efuller1101@gmail.com>
2022-01-06 19:46:29 +1100

    removed uncessary stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
 
-# def set_locale
-#   if user_signed_in?
-#     I18n.locale = current_user.language
-#   else
-#     I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
-#   end
-# end
+def set_locale
+    I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+end
 
 def set_locale
   I18n.locale = :es
@@ -18,9 +14,3 @@ end
 def locale_from_header
   request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
 end
-
-# private
-
-# def configure_permitted_parameters
-#   devise_paramter_sanitizer.permit()
-# end
