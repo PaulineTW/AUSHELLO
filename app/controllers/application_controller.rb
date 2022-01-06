ee6634dca4f39916c7251264005af75b3739b551
elliott fuller <efuller1101@gmail.com>
2022-01-06 20:32:57 +1100

    added translations to resource window

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ def set_locale
     I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
 end
 
-# def set_locale
-#   I18n.locale = :ar
-# end
+def set_locale
+  I18n.locale = :ar
+end
 
 def locale_from_header
   request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
