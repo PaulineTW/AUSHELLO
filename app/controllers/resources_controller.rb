52cce3143cb05eb82ed5436b559f50d10ee21558
pauline rouger <paulinerouge@hotmail.com>
2022-01-15 08:35:58 +0800

    only have category search now

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,38 +3,9 @@ before_action :set_locale
 
 def index
   @tags = ActsAsTaggableOn::Tag.all
-  # if params[:keywords]
-  # @filters = params[:keywords]
-  # @resources = @filters.nil? ? Resource.all : Resource.where(id: Resource.all.tagged_with(@filters, any: true).reject(&:blank?).map(&:id))
-  #     @markers = @resources.geocoded.map do |resource|
-  #     {
-  #       lat: resource.latitude,
-  #       lng: resource.longitude,
-  #       info_window: render_to_string(partial: "info_window", locals: { resource: resource }),
-  #     }
-  #   end
-
-
-  if params[:query].present?
-      @resources = Resource.search(params[:query])
-      #   @markers = @resources.geocoded.map do |resource|
-      # {
-      #   lat: resource.latitude,
-      #   lng: resource.longitude,
-      #   info_window: render_to_string(partial: "info_window", locals: { resource: resource }),
-      # }
-      # end
-
-  else
-    @resources = Resource.all
-    # @markers = @resources.geocoded.map do |resource|
-    #   {
-    #     lat: resource.latitude,
-    #     lng: resource.longitude,
-    #     info_window: render_to_string(partial: "info_window", locals: { resource: resource }),
-    #   }
-    # end
-  end
+  if params[:keywords]
+  @filters = params[:keywords]
+  @resources = @filters.nil? ? Resource.all : Resource.where(id: Resource.all.tagged_with(@filters, any: true).reject(&:blank?).map(&:id))
       @markers = @resources.geocoded.map do |resource|
       {
         lat: resource.latitude,
@@ -42,6 +13,17 @@ def index
         info_window: render_to_string(partial: "info_window", locals: { resource: resource }),
       }
     end
+
+  else
+    @resources = Resource.all
+      @markers = @resources.geocoded.map do |resource|
+      {
+        lat: resource.latitude,
+        lng: resource.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { resource: resource }),
+      }
+    end
+  end
 end
 
   def new
