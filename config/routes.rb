d94b3c0bbf7c45322193d1feb98aa7c995457ad2
pauline rouger <paulinerouge@hotmail.com>
2022-01-15 09:47:04 +0800

    clean code

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-resources :resources do
-  # put :favourite, on: :member
-  resources :favourites, only: %i[create]
-end
+  resources :resources do
+    resources :favourites, only: %i[create]
+  end
 
-resources :favourites, only: [:destroy]
+  resources :favourites, only: [:destroy]
 
-  resources :users, only: [:show] do
-    resources :dashboard, only: [:index]
-end
-
+    resources :users, only: [:show] do
+      resources :dashboard, only: [:index]
+  end
 end
