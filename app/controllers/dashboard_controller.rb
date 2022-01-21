4dd673732e7ddf4ced09eb4ca4dca49b06dfab99
pauline rouger <paulinerouge@hotmail.com>
2022-01-21 16:39:22 +0800

    Drop Table + model favorites

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,16 +1,12 @@
 class DashboardController < ApplicationController
-before_filter :admin_user,     only: [:index]
+# before_action :admin_user,     only: [:index]
 
-def admin_user
-    redirect_to(root_url) unless current_user.present? && current_user.admin?
-end
+# def admin_user
+#     redirect_to(root_url) unless current_user.present? && current_user.admin?
+# end
 
 
   def index
-    @bookings = Booking.where(user: current_user)
-    @my_kitchens_bookings = current_user.kitchens.flat_map{|kitchen|kitchen.bookings}
-    @my_kitchens = Kitchen.where(user: current_user)
-
   end
 
 end
