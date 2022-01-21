48d247757eee52722657ec30eb8a7d0d6b747df7
pauline rouger <paulinerouge@hotmail.com>
2022-01-21 16:26:48 +0800

    setup admin + db:seed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,16 @@
 class DashboardController < ApplicationController
+before_filter :admin_user,     only: [:index]
 
-  # def index
+def admin_user
+    redirect_to(root_url) unless current_user.present? && current_user.admin?
+end
 
-  # end
+
+  def index
+    @bookings = Booking.where(user: current_user)
+    @my_kitchens_bookings = current_user.kitchens.flat_map{|kitchen|kitchen.bookings}
+    @my_kitchens = Kitchen.where(user: current_user)
+
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :password, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :resources
