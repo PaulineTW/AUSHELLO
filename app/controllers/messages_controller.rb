ff946f75150ec31c3be22852eb392fc6857bbc79
elliott fuller <efuller1101@gmail.com>
2022-01-27 21:21:56 +1100

    messages, resource, status appear on admin dash

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,21 @@
 class MessagesController < ApplicationController
-    def new
-      @message= Message.new
-      @resource = Resource.find(params[:resource_id])
-    end
+  def new
+    @message= Message.new
+    @resource = Resource.find(params[:resource_id])
+  end
 
-    def create
-        @message = Message.new(message_params)
-        @message.user = current_user
-        @message.resource = Resource.find(params[:resource_id])
-        if @message.save
-          redirect_to resources_path
-        end
-    end
+  def create
+      @message = Message.new(message_params)
+      @message.user = current_user
+      @message.resource = Resource.find(params[:resource_id])
+      if @message.save
+        redirect_to resources_path
+      end
+  end
 
-    private
+  private
 
-    def message_params
-        params.require(:message).permit(:content)
-    end
+  def message_params
+      params.require(:message).permit(:content)
+  end
 end
