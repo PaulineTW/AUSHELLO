41281fe3bde1292efaeaf258e370c1bf11d631c3
pauline rouger <paulinerouge@hotmail.com>
2022-01-28 00:08:47 +0800

    added issue colum to schema, in update form and view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,6 +23,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-      params.require(:message).permit(:content)
+      params.require(:message).permit(:content, :issue)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_041059) do
+ActiveRecord::Schema.define(version: 2022_01_27_160358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_041059) do
     t.bigint "resource_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "issue"
     t.index ["resource_id"], name: "index_messages_on_resource_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
